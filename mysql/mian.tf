98c049fd431fd469402c74324100e82e7d86e399
Yogesh Mangela <yogmangela@hotmail.co.uk>
2020-05-12 12:51:07 +0100

    added mysql module

---
+++ mysql/mian.tf
@@ -0,0 +1,12 @@
+
+
+resource "aws_db_instance" "default" {
+  allocated_storage    = var.allocated
+  storage_type         = var.type
+  engine               = var.eng
+  engine_version       = var.eng_ver
+  instance_class       = var.instance_class
+  name                 = var.db_name
+  username             = var.usr
+  password             = var.pwd
+}
---
+++ mysql/variables.tf
@@ -0,0 +1,49 @@
+
+variable "allocated" {
+    type= number
+    default = 20
+} 
+variable "instance_class" {
+  type=string
+  default="db.t2.micro"
+}
+variable "usr" {
+  type=string
+  default="admin"
+}
+variable "pwd" {
+  type=string
+  default="admin2020"
+}
+
+variable "type" {
+    type= string
+    default = "gp2"
+} 
+variable "eng" {
+  type=string
+  default="mysql"
+}
+variable "eng_ver" {
+  type=string
+  default="5.7"
+}
+
+variable "db_name" {
+  type=string
+  default="mysqldb-test"
+}
+
+
+  /*
+  allocated_storage    = 20
+  storage_type         = "gp2"
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "db.t2.micro"
+  name                 = "mydb"
+  username             = "foo"
+  password             = "foobarbaz"
+  */
+
+  
